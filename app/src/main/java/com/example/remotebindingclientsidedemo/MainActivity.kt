532d897bbf02dfdf063d53450daec4dd43e0e2d6
Samriddha <samriddha97@gmail.com>
2023-01-05 18:06:27 +0530

    remote binding setup

--- app/src/main/java/com/example/remotebindingclientsidedemo/MainActivity.kt
+++ app/src/main/java/com/example/remotebindingclientsidedemo/MainActivity.kt
@@ -1,42 +1,103 @@
 package com.example.remotebindingclientsidedemo
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.*
-import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.remotebindingclientsidedemo.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var randomNumber = 0
+    private val TAG = "MyService"
+    private val GET_RANDOM_NUMBER_FLAG = 0
+    private var isBound=false
+    private var randomNumberRequestMessenger: Messenger? = null
+    private var randomNumberReceiveMessenger:Messenger? = null
+    private var randomNumberServiceConnection:ServiceConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            randomNumberRequestMessenger = null
+            randomNumberReceiveMessenger = null
+            isBound = false
+        }
+
+        override fun onServiceConnected(arg0: ComponentName, binder: IBinder) {
+            randomNumberRequestMessenger = Messenger(binder)
+            randomNumberReceiveMessenger = Messenger(ReceiveRandomNumberHandler())
+            isBound = true
+        }
+    }
+    private var serviceIntent:Intent?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        serviceIntent = Intent()
+        serviceIntent?.component = ComponentName(
+            "com.example.remotebindingservicesidedemo",
+            "com.example.remotebindingservicesidedemo.MyService"
+        )
+
+        binding.button.setOnClickListener {
+            bindToRemoteService()
+        }
+        binding.button2.setOnClickListener {
+            unbindFromService()
+        }
+        binding.button3.setOnClickListener {
+            getRandomNumber()
+        }
 
     }
 
-    companion object {
-        private val TAG = "MyService"
-        private const val GET_RANDOM_NUMBER_FLAG = 0
+    private fun unbindFromService() {
+        if(isBound){
+            unbindService(randomNumberServiceConnection)
+            isBound=false
+            Toast.makeText(this,"Service Unbound",Toast.LENGTH_SHORT).show();
+        }
+    }
 
-        class ReceiveRandomNumberHandler : Handler(Looper.getMainLooper()) {
+    private fun bindToRemoteService() {
+        bindService(serviceIntent, randomNumberServiceConnection, BIND_AUTO_CREATE);
+        Toast.makeText(this,"Service bound",Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getRandomNumber(){
+        if (isBound) {
+            val requestMessage = Message.obtain(null, GET_RANDOM_NUMBER_FLAG)
+            requestMessage.replyTo = randomNumberReceiveMessenger
+            try {
+                randomNumberRequestMessenger?.send(requestMessage)
+            } catch (e: RemoteException) {
+                e.printStackTrace()
+            }
+        } else {
+            Toast.makeText(this, "Service Unbound, can't get random number", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private inner class ReceiveRandomNumberHandler : Handler(Looper.getMainLooper()) {
             override fun handleMessage(msg: Message) {
+                randomNumber = 0
                 when (msg.what) {
                     GET_RANDOM_NUMBER_FLAG -> {
-                        val messageSendRandomNumber = Message.obtain(null, GET_RANDOM_NUMBER_FLAG)
-                        messageSendRandomNumber.arg1 = getRandomNumber()
-                        try {
-                            msg.replyTo.send(messageSendRandomNumber)
-                        } catch (e: RemoteException) {
-                            Log.i(TAG, "" + e.message)
-                        }
+                        randomNumber = msg.arg1
+                        binding.tvNumber.text = "Random Number : $randomNumber"
                     }
                 }
                 super.handleMessage(msg)
             }
-        }
-
-        fun getRandomNumber(): Int {
-            return mRandomNumber
-        }
-
-        private var mRandomNumber = 0
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
 }
